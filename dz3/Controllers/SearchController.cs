4bf84cc83ec108c342fc7c65103708c030596efd
Max1mus <perepichkamax10@gmail.com>
2026-09-02 23:38:41 +0300

    Add project files.

---
+++ dz3/Controllers/SearchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dz3.Controllers
+{
+    public class SearchController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index(string query)
+        {
+            ViewBag.Query = query;
+
+            return View();
+        }
+    }
+}
